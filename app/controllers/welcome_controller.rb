00389c59ccd7e4b392e3c91c7d3cb2401d09235f
marinacor1 <marinaspam1234@gmail.com>
2016-06-24 23:07:58 -0600

    Basic button and drop down created on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def show
+    @locations = Location.all
   end
 end
--- spec/features/user_can_visit_a_city_through_landing_page_drop_down_spec.rb
+++ spec/features/user_can_visit_a_city_through_landing_page_drop_down_spec.rb
@@ -5,11 +5,12 @@ feature "user can visit a city through landing page drop down" do
     city = Location.create(name: "Denver")
 
     visit root_path
-
-    select("Denver, Colorado")
+    
+    click_on "Where do you want to go?"
+    select("Denver")
     click_button "Submit"
 
-    expect(current_path).to eq city_path(city)
+    expect(current_path).to eq location_path(city)
 
   end
 end
