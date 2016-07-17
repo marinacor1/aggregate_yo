4b4a989e8764f32c273c4a9511837af87d18ea80
marinacor1 <marinaspam1234@gmail.com>
2016-07-17 16:19:07 -0600

    Services created and locations controller created

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,16 @@ class Item
   attr_reader :service
 
   def initialize
-    @service = FHServices.new
+    @service ||= FHServices.new
   end
 
-  def items_by_location(zipcode) #passed in through geofilter
-    binding.pry
+  def self.items_by_location #passed in through geofilter
+    all_items = FHServices.new.items_hash
+    all_items[:items].map do |item|
+      {name: item[:name], location: item[:location],
+        image: item[:images]}
+    end
+
   end
 
 end
