1e8430205fd39cfa4b7344be7f4d09687d4be0f3
marinacor1 <marinaspam1234@gmail.com>
2016-07-17 15:22:39 -0600

    Api key works

--- app/services/fh_services.rb
+++ app/services/fh_services.rb
@@ -1,14 +1,12 @@
 class FHServices
   def initialize
-    @connection = Faraday.new(url: "https://demo.fareharbor.com/api/external/v1/companies/marina/")
+    @connection = Faraday.new(url: "https://demo.fareharbor.com/api/external/v1/companies/")
   end
 
   def get_company
-#  curl -X GET "https://demo.fareharbor.com/api/external/v1/companies/?api-app=9d524aaa-aed7-462f-9133-f6a25a88d431&api-user=8def417d-b7e9-4364-a870-3e2a195b105b"
-
+    @connection.get "/?api-app="+"#{ENV["APPKEY"]}"+"&api-user="+"#{ENV["user_key"]}"
   end
 
   def get_item
-#     curl -X GET "https://demo.fareharbor.com/api/external/v1/companies/marina/items/?api-app=9d524aaa-aed7-462f-9133-f6a25a88d431&api-user=8def417d-b7e9-4364-a870-3e2a195b105b"
   end
 end
