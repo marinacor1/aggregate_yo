742b28fbc8fcbb80af44ed2ee9bac5a316ea5909
marinacor1 <marinaspam1234@gmail.com>
2016-07-18 18:15:33 -0600

    Add js to user can visit feature

--- spec/features/user_can_visit_a_city_through_landing_page_drop_down_spec.rb
+++ spec/features/user_can_visit_a_city_through_landing_page_drop_down_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "user can visit a city through landing page drop down" do
-  it "shows a dropdown menu" do
+  it "shows a dropdown menu", js:true do
     city = Location.create(name: "Denver")
 
     visit root_path
