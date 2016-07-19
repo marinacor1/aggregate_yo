3bd894784a975555b0eb17bbb59d566bf90d3b53
marinacor1 <marinaspam1234@gmail.com>
2016-07-19 11:50:42 -0600

    Add slug to seed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessor :name
 
-  validates_presence_of :slug
-  
-  def slug
-    name.downcase.gsub(" ", "-")
-  end
-
   def to_param
     "#{name}"
   end
