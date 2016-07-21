651edff1bbf2d9d7e0ca92f248087a53883dc44a
marinacor1 <marinaspam1234@gmail.com>
2016-07-21 13:48:41 -0600

    Add method for finding items for a company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,11 @@
 class CompaniesController < ApplicationController
 
   def index
-    @companies = Company.company_save
+    @companies = Company.all
   end
 
   def show
     @company = Company.find(params[:id])
+    @items = Item.items_by_company(@company[:shortname])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,19 @@ class Item
       {name: item[:name], location: item[:location],
         image: item[:images]}
     end
-    citie_groups = grouped.group_by {|hash| hash[:location]}
+    cities_groups = grouped.group_by {|hash| hash[:location]}
 binding.pry
     # {"Miami, FL"=>[{:name=>"Marina's Bachata Dance Classes", :location=>"Miami, FL", :image=>[]}]}
+  end
+
+  def self.items_by_company(shortname)
+    all_items = FHServices.new.items_hash(shortname)
+    grouped = all_items[:items].map do |item|
+      binding.pry
+      {name: item[:name], location: item[:location],
+        image: item[:url]}
+    end
+
   end
 
 end
