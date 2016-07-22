fbbbf015b318c5bd102a96e47102f5c0d8eda84b
marinacor1 <marinaspam1234@gmail.com>
2016-07-22 00:16:26 -0600

    Item methods refactored to have more clarity

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,13 +6,9 @@ class Company < ActiveRecord::Base
     all_companies.each do |comp|
       Company.first_or_create(name: comp[:name], shortname: comp[:shortname])
     end
-    #account for deleted companies
+    #how do I account for deleted companies?
   end
 
-
-  def self.company_save_location
-
-  end
 
   def self.find_by_location(zip)
     Company.where(location: zip)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,19 @@
-class Item
-  attr_reader :service
-
-  def initialize
-    @service ||= FHServices.new
-  end
+class Item < ActiveRecord::Base
 
   def self.items_by_location(location)
     all_companies = Company.find_by_location(location)
       all_companies.each do |shortname|
         all_items = FHServices.new.items_hash(shortname)
       end
-      grouped = all_items[:items].map do |item|
-        {name: item[:name], location: item[:location],
-          image: item[:images]}
-      end
+    grouped = organize_items(all_items)
     cities_groups = grouped.group_by {|hash| hash[:location]}
-    # {"Miami, FL"=>[{:name=>"Marina's Bachata Dance Classes", :location=>"Miami, FL", :image=>[]}]}
+  end
+
+  def organize_items(all_items)
+    all_items[:items].map do |item|
+      {name: item[:name], location: item[:location],
+      image: item[:images]}
+    end
   end
 
   def self.items_by_company(shortname)
@@ -29,8 +27,20 @@ class Item
   def self.item_save
     all_companies = Company.all
     companies = all_companies.map do |comp|
-      {items: FHServices.new.items_hash(comp[:shortname]), shortname: comp[:shortname]}
+      c_items = FHServices.new.items_hash(comp[:shortname])
+      save_items_to_database(c_items)
+      {items: c_items, shortname: comp[:shortname]}
     end
+    save_location(companies)
+  end
+
+  def save_item_to_database(c_items)
+    c_items.each do |item|
+      Item.first_or_create(name: item[:name], location: item[:location], company_id: comp.id)
+    end
+  end
+
+  def save_location(companies)
     companies.each do |c|
       c[:items][:items].each do |item|
         comp = Company.find_by(shortname: c[:shortname])
