73eb88f0b5c4753bed32f4c2aad5fb2888fd9e0a
marinacor1 <marinaspam1234@gmail.com>
2016-07-21 19:45:58 -0600

    Whenever spec updated with information from blog

--- spec/whenever_spec.rb
+++ spec/whenever_spec.rb
@@ -1,12 +1,10 @@
-require 'rails_helper'
-
-describe Whenever Schedule, do
+describe Whenever do
   let(:whenever) do
     Whenever::Company.new(file: Rails.root.join("config", "schedule.rb").to_s)
   end
 
-  it "schedules updates to company database" do
+  it "schedules the sending of challenge submission digests" do
     expect(whenever).to schedule_rake("company_update")
-    .every(1.day)
+      .every(1.day)
   end
 end
