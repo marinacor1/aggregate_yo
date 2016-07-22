fc56d023e78529abc81c099f1e8f4245b2c80178
marinacor1 <marinaspam1234@gmail.com>
2016-07-21 22:26:45 -0600

    Location show is working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,11 +4,12 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = Location.find(params['id']).name
-    # params[location] will be passed into items_by_location and
-    # only items within a certain location will be displayed
-    city_group = Item.items_by_location(@location)
-    @items = city_group[@location]
+    @location = Location.find(params['id'])
+    # location_name = @location.name
+    # # params[location] will be passed into items_by_location and
+    # # only items within a certain location will be displayed
+    # city_group = Item.items_by_location(location_name)
+    # @items = city_group[@location]
   end
 
 end
