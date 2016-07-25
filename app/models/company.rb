d14038510c908383d1ba7f9fc80fa8fd251023d3
marinacor1 <marinaspam1234@gmail.com>
2016-07-24 22:35:52 -0600

    Found bug in company saving

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,7 +4,9 @@ class Company < ActiveRecord::Base
   def self.company_save
     all_companies = FHServices.new.companies_hash[:companies]
     all_companies.each do |company|
+      binding.pry
       Company.first_or_create(name: company[:name], shortname: company[:shortname])
+      #TODO this is currently not saving all companies
     end
     #how do I account for deleted companies?
   end
