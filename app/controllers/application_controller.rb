064894a9022b0f0da508dfa529d1998e7ea4a8fd
marinacor1 <marinaspam1234@gmail.com>
2016-07-24 23:38:27 -0600

    Moving updates to application controller so any page can update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :check_updates
+
+  def check_updates
+    # current_time = Time.now
+    # last_time = Item.last.updated_at
+    # if last_time.nil? || (last_time - current_time) > -20
+      update_companies_and_items
+    # end
+  end
+
+  def update_companies_and_items
+    Company.company_save
+    Item.item_save
+  end
+
 end
