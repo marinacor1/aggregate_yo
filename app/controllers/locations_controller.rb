49bfaeecb5275e4aa9acb2c4199001637f968b7f
marinacor1 <marinaspam1234@gmail.com>
2016-07-24 23:37:05 -0600

    city show no longer calls API but rather database

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,15 +5,6 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    #TODO currently the relationship is not built because
-    #there is no location_id for items
-    #a possible solution is to check to see if location exists
-    #if so add that as location id
-    #else create a new location and then add that as location id
-    
-    # location_name = @location[:name]
-    # city_group = Item.items_by_location(@location[:name])
-    # @location_items = city_group[location_name]
   end
 
 end
