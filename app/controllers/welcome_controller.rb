83b458179b102f2a6704552b83edf42ca64b4cbf
marinacor1 <marinaspam1234@gmail.com>
2016-07-24 23:36:16 -0600

    Refactoring and deleting comments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -18,7 +18,5 @@ class WelcomeController < ApplicationController
   def show
     locations = Location.all
     @locations = locations.order(:name)
-    #the list should be a filter and the list should be pulled from
-    #a possible google maps api list
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,31 +2,12 @@ class Item < ActiveRecord::Base
   belongs_to :company
   belongs_to :location
 
-  # def self.items_by_location(location)
-  #   all_companies_in_area = Company.find_by_location(location)
-  #     all_items = all_companies_in_area.map do |company|
-  #       FHServices.new.items_hash(company)
-  #     end
-  #   grouped = self.organize_items(all_items) #TODO look at data for multiples
-  #   cities_groups = grouped.group_by {|hash| hash[:location]}
-  # end
-
-  # def self.organize_items(all_items)
-  #   binding.pry
-  #   cleaned_all_items = all_items.first[:items]
-  #   cleaned_all_items.map do |item|
-  #     {name: item[:name], location: item[:location],
-  #     image: item[:image_cdn_url]}
-  #   end
-  # end
-
   def self.item_save
     companies = Company.all.map do |comp|
       self.format_and_save_items(comp)
     end
     self.save_location(companies)
   end
-
 
   def self.format_and_save_items(comp)
     companys_items = FHServices.new.items_hash(comp[:shortname])
@@ -36,17 +17,21 @@ class Item < ActiveRecord::Base
 
   def self.save_items_to_database(companys_items, comp)
     companys_items.each do |company|
-      item_details = company[1] #hacky way to get access to details
+      item_details = company[1]
         item_details.each do |item|
-        # unless Item.find_by(name: item_details[:name])
-          new_item = Item.create(name: item[:name],
-                               company_id: comp.id,
-                               image: item[:image_cdn_url])
-          specific_location = Location.find_by(name: item[:location])
-          self.create_location_reference(item[:location], new_item, specific_location)
+          self.saving_iteration(item)
         end
         #TODO add regex to clean up entries
     end
+  end
+
+  def self.saving_iteration(item)
+    unless Item.find_by(name: item_details[:name])
+      new_item = Item.create(name: item[:name],
+                           company_id: comp.id,
+                           image: item[:image_cdn_url])
+      specific_location = Location.find_by(name: item[:location])
+      self.create_location_reference(item[:location], new_item, specific_location)
   end
 
   def self.save_location(items_information)
